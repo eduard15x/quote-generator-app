7cabb9fb0e8c15b6274805f25a044479a695bc1a
Precup Eduard <precupeduard99@gmail.com>
2023-01-21 02:01:05 +0200

    Update project
    -add transition to display text
    -add DOM manipulation for elements
    -create event listener that renders text

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
+const container = document.querySelector('body');
+const generateBtn = document.querySelector('.quote-generate-btn');
+const quoteText = document.querySelector('.quote-text');
+const quoteAuthor = document.querySelector('.quote-author');
+
+// Event listener for the button to render random quote
+generateBtn.addEventListener('click', (e) => {
+    getData('https://quotable.io/quotes?author=albert-einstein');
+    e.target.setAttribute('disabled', true);
+    container.classList.add('active');
+});
+
+// Function that fetch and return the data to JSON model 
 const getData = async function(url) {
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data.results);
-    console.log(data.results.length);
     let quotesArray = data.results;
+    let randomIndex = generateRandomNr(quotesArray);
 
-    let randomNumber = (Math.random() * (quotesArray.length - 1)).toFixed();
-    console.log(randomNumber);
+    quoteText.innerHTML = `"${quotesArray[randomIndex].content}"`;
+    quoteAuthor.innerHTML = `- ${quotesArray[randomIndex].author}`;
 };
 
-getData('https://quotable.io/quotes?author=albert-einstein');
-
-
-
+function generateRandomNr(arr) {
+    return (Math.random() * (arr.length - 1)).toFixed()
+};
