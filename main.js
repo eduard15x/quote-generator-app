04459231854f2e60594d4598a648813ae614fb2e
Precup Eduard <precupeduard99@gmail.com>
2023-01-24 12:46:49 +0200

    Update JS fetch method
    Implement the try...catch method for a better usage and update the link with a complexer one

--- main.js
+++ main.js
@@ -5,21 +5,37 @@ const quoteAuthor = document.querySelector('.quote-author');
 
 // Event listener for the button to render random quote
 generateBtn.addEventListener('click', (e) => {
-    getData('https://quotable.io/quotes?author=albert-einstein');
+    getD();
     e.target.setAttribute('disabled', true);
     container.classList.add('active');
 });
 
 // Function that fetch and return the data to JSON model 
-const getData = async function(url) {
-    let response = await fetch(url);
-    let data = await response.json();
-    let quotesArray = data.results;
-    let randomIndex = generateRandomNr(quotesArray);
+// const getData = async function(url) {
+//     let response = await fetch(url);
+//     let data = await response.json();
+//     let quotesArray = data;
+//     let randomIndex = generateRandomNr(quotesArray);
+//     console.log(randomIndex)
 
-    quoteText.innerHTML = `"${quotesArray[randomIndex].content}"`;
-    quoteAuthor.innerHTML = `- ${quotesArray[randomIndex].author}`;
-};
+//     quoteText.innerHTML = `"${quotesArray[randomIndex].text}"`;
+//     quoteAuthor.innerHTML = `- ${quotesArray[randomIndex].author}`;
+// };
+
+const getD = async function() {
+    let url = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        let quotesArray = data;
+        let randomQuote = generateRandomNr(quotesArray);
+
+        quoteText.innerHTML = `"${quotesArray[randomQuote].text}"`;
+        quoteAuthor.innerHTML = `- ${quotesArray[randomQuote].author}`;
+    } catch(err) {
+        console.log(`Unfortunately this link is broken ${url}`)
+    }
+}
 
 function generateRandomNr(arr) {
     return (Math.random() * (arr.length - 1)).toFixed()
